718a209889a0aa1ab53e25454ee43653a4668ee2
Lenazaghloul <lenazaghloul@gmail.com>
2023-11-01 19:37:32 +0200

    javaScript syntax

---
+++ index.js
@@ -0,0 +1,168 @@
+// alert('hello world')
+console.log('hello world')
+console.warn('Warning message')
+console.error('error message')
+console.clear()
+
+// var let const
+
+// let age = 20
+// age = 30
+// console.log(age)
+
+
+// const num =50
+// num =60
+// console.log(num)
+
+
+//Data Types 
+// String , Number , Boolean , Undefined , null
+
+// const name ='lena'
+// console.log(typeof(name))
+// const age = 20
+// console.log(typeof(age))
+// const rate = 6.8
+// console.log(typeof(rate))
+// const isWorking = false
+// console.log(typeof(isWorking))
+// let x = undefined
+// console.log(typeof(x))
+
+
+// console.log('My name is ' + name + ' and age is ' + age)
+
+// console.log(`My name is ${name} and age is ${age}`)
+
+// const l = 'LENA AMR'
+// console.log(l.toUpperCase())
+// console.log(l.toLowerCase())
+// console.log(l.split())
+
+
+//Arrays
+
+// const z = ['fruits', 'vegetables', 10, 4.8, true]
+// console.log(z)
+
+// const fruits = ['apple', 'banana', 'orange']
+// fruits.push('mango')
+// // fruits[4] = 'grapes'
+// fruits.unshift('orange')
+// fruits.pop()
+// console.log(fruits.indexOf('apple'))
+// console.log(fruits)
+
+// const person = {
+//     firstName: 'Lena',
+//     lastName: 'Amr',
+//     age: 20,
+//     hobbies: ['music', 'watching', 'sports'],
+//     address: {
+//         street: '123',
+//         country: 'egypt',
+//         city: 'cairo',
+//     }
+    
+// }
+
+// person.email = 'lena@gmail.com'
+// person.phone= 1264654
+// console.log(person.hobbies)
+// console.log(person.address)
+// console.log(person)
+
+
+// const todos =[
+//     {
+//         id: 1,
+//         text: 'watching',
+//         isCompleted: true
+//     },
+//     {
+//         id: 2,
+//         text: 'Meeting',
+//         isCompleted: true
+//     }, {
+//         id: 3,
+//         text: 'Cooking',
+//         isCompleted: false
+//     },
+
+// ]
+
+
+
+// console.log(todos[0])
+// const todoJson = JSON.stringify(todos)
+// console.log(todoJson)
+
+// for (let i =0 ; i < todos.length ; i++){
+//     console.log(todos[i].isCompleted)
+// }
+
+
+// for (let t of todos){
+//     console.log(t.id)
+// }
+
+//for while
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`from 1 to 9 :${i}`)
+// }
+
+
+
+// let x = 0;
+// while (x < 10) {
+//     console.log(`${x}`);
+//     x++ ;
+// }
+
+
+// const x = 'lena';
+// if (x < 10) {
+//     console.log('x less than 10');
+// }
+// else if (x > 10) {
+//     console.log('x greater than 10');
+// }
+// else {
+//     console.log('x is not a number');
+// }
+
+
+// const x = 15;
+// const color = x > 10 ? 'red' : 'green';
+// // console.log(color);
+
+// switch (color) {
+//     case 'green':
+//         console.log('green color');
+//         break;
+//     case 'red':
+//         console.log('red color');
+//         break;
+//     default:
+//         console.log('unknown color');
+//         break;
+
+// }
+
+
+//function
+// function num (num1, num2) {
+//     console.log(num1 + num2);
+// }
+// num(10,10)
+
+
+
+// function num (num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(num(10,10));
+
+
